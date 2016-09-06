14c929cbf00cd291f6f87b7e0a7face276b2a70c
jona4759 <jona4759@EAL-00287>
2016-09-06 10:36:41 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _01_Calculator
+{
+    class Calculator
+    {
+        internal static int Add(params int[] values)
+        {
+            int length = values.Length;
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                sum = values[i] + sum;
+            }
+           return sum;
+        }
+
+        internal static int Subtract(params int[] values)
+        {
+            int length = values.Length;
+            int sum = values[0];
+            for (int i = 1; i < length; i++)
+            {
+                sum = sum - values[i];
+            }
+            return sum;
+        }
+
+        internal static object Sum(int[] values)
+        {
+            int length = values.Length;
+            int sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                sum = values[i] + sum;
+            }
+            return sum;
+        }
+
+        internal static int Multiply(params int[] values)
+        {
+            int length = values.Length;
+            int sum = values[0];
+            for (int i = 1; i < length; i++)
+            {
+                sum = values[i] * sum;
+            }
+            return sum;
+        }
+
+        internal static double Power(int Base, int Expo)
+        {
+            return Math.Pow(Base, Expo);
+        }
+
+        internal static int Factorial(int Fact)
+        {
+            int sum = Fact;
+            for (int i = Fact - 1; i > 0; i--)
+            {
+                sum = sum * i;
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ 01_Calculator/CalculatorTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_Calculator
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Assert.AreEqual(0, Calculator.Add(0, 0));
+            Assert.AreEqual(104, Calculator.Add(2, 2, 100));
+            Assert.AreEqual(17, Calculator.Add(2, 6, 4, 5));
+        }
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.AreEqual(6, Calculator.Subtract(10, 4));
+        }
+        [TestMethod]
+        public void Sum()
+        {
+            //Arrange
+            int[] numbers = new int[8];
+            Assert.AreEqual(0, Calculator.Sum(numbers));
+
+            numbers[0] = 7;
+            Assert.AreEqual(7, Calculator.Sum(numbers));
+
+            numbers[0] = 7;
+            numbers[1] = 11;
+            Assert.AreEqual(18, Calculator.Sum(numbers));
+
+        }
+        [TestMethod]
+        public void Multiply()
+        {
+            Assert.AreEqual(24, Calculator.Multiply(2,3,4));
+            Assert.AreEqual(10000, Calculator.Multiply(10, 10, 10, 10));
+        }
+        [TestMethod]
+        public void Power()
+        {
+            Assert.AreEqual(10000, Calculator.Power(10, 4));
+            Assert.AreEqual(64, Calculator.Power(2, 6));
+        }
+        [TestMethod]
+        public void Factorial()
+        {
+            Assert.AreEqual(120, Calculator.Factorial(5));
+        }
+    }
+}
